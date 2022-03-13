e9cb6e2baa0d6c46a2b1278881e35f9c7ef9f2d8
Jude <lovespictures2@gmail.com>
2022-03-13 21:06:53 +0000

    updated search areaa

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 //You can edit ALL of the code here
-//using cashe to mitigate the delay in getting the data
+//using cache to mitigate the delay in getting the data
 //async await - modern way of fetching data
 
 //Get the data
-function getData() {
+function getData(showID) {
   // to the URL - object where the value is not yey defined
-  fetch("https://api.tvmaze.com/shows/82/episodes")
+  //fetching the main URL and the shows by the show ID
+  fetch("https://api.tvmaze.com/shows/" + showID + "/episodes")
     //converting the object to JSON
     .then((response) => response.json())
     //waiting the the data to be finished Promise?
     .then((data) => {
-      //calling the makePageForEpisoned on the JSon
+      //calling the makePageForEpisode on the JSon
       makePageForEpisodes(data);
       // return data;
     })
-    //this catch method is called when the promise is not sucessful. 200-299 ? > 400 ??
+    //this catch method is called when the promise is not successful. 200-299 ? > 400 ??
     .catch((e) => console.log(e));
 }
 // console.log(getData());
@@ -22,8 +23,27 @@ function getData() {
 let currentEpisodes = [];
 
 function setup() {
-  //get the data
-  getData();
+  //store the allshows data
+  let allshowsData = getAllShows();
+
+  for (let show of allshowsData) {
+    //copy a template from the HTML for each show and clone it
+    let showOption = document.querySelector(`.template-option`).cloneNode(true);
+
+    //make the template visible (this needs to remain as a placeholder for javascript)
+    showOption.classList.remove(`template-option`);
+
+    //update show name
+    showOption.innerHTML = show.name;
+    // this the the dropdown values
+    showOption.setAttribute(`value`, show.id); //not visible
+
+    // add/append other elements in the card
+    document.querySelector(`#showList`).appendChild(showOption);
+  }
+
+  //get the data / test sho id's here
+  getData(33);
 }
 
 //async do this at the same time
@@ -32,15 +52,21 @@ async function makePageForEpisodes(episodeList) {
   // const rootElem = document.getElementById("root");
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
+  //clear the current episodes options on the page ------------
+  document.querySelectorAll(`.episodeOption`).forEach((temp) => {
+    //if this element does not include the template class remove it
+    if (!temp.classList.contains(`template-option`)) temp.remove();
+  });
+
   // CARD
-  //for in - to loop over the episodes
+  //for of - to loop over the episodes
   episodes = episodeList;
   for (let episode of episodeList) {
     // console.log(episode);
-
+    // CARD---------------------------------------------------------------------------
     // create a card and clone it
-    let card = document.querySelector(`.card`).cloneNode(true);
-    //removing the template visibility (this needs to remain as a placeholder for javascript)
+    let card = document.querySelector(`.template`).cloneNode(true);
+    //make the template visible (by removing the CSS class - this needs to remain as a placeholder for javascript)
     card.classList.remove(`template`);
 
     //update text
@@ -56,20 +82,74 @@ async function makePageForEpisodes(episodeList) {
     card.querySelector(`.season-episode`).innerHTML =
       `S` + episode.season + `E` + episode.number;
 
-    //update image
+    //update image for the card
     card.querySelector(`img`).src = episode.image.medium;
-    // append other elements in the card
+    // add/append other elements in the card
     document.querySelector(`.wrapper`).appendChild(card);
+
+    // EPISODE OPTION -----------------------------------------------------------------
+    //create a dropdown option for each episode
+    //copy a template from the HTML for each episode and clone it
+    let episodeOption = document
+      .querySelector(`.template-option`)
+      .cloneNode(true);
+
+    //adding the episodeOption class - so it can be removed in future
+    episodeOption.classList.add(`episodeOption`);
+    //make the template visible (by removing the CSS class - this needs to remain as a placeholder for javascript)
+    episodeOption.classList.remove(`template-option`);
+
+    //update episode name
+    episodeOption.innerHTML =
+      `S` + episode.season + `E` + episode.number + `⠀⠀⠀` + episode.name;
+
+    // this the the dropdown values
+    episodeOption.setAttribute(`value`, episode.id); //not visible
+
+    // add/append other elements in the card
+    document.querySelector(`#episodeList`).appendChild(episodeOption);
   }
+  // update display for the the number of visible episodes
+  updateDisplay();
 }
 
-//Show Selector
-//filter shows list and search
+// call the shows
+console.log(`here`, getAllShows());
+//select an episode
+document.querySelector(`#episodeList`).addEventListener("input", function (e) {
+  for (let episode of episodes) {
+    //if episode id equals the one selected, display it
+    //this.value = the input  //this.value = string and episodes.id = number therefore === does not work
+    if (this.value == episode.id) {
+      //remove the - hide class - this knows it is a class so you do not need the `.`
+      //Using querySelector with IDs that are numbers
+      document.querySelector(`#episode` + episode.id).classList.remove(`hide`);
+    } else {
+      //adds the - hide class
+      document.querySelector(`#episode` + episode.id).classList.add(`hide`);
+    }
+  }
+  // update display for the the number of visible episodes
+  updateDisplay();
+});
+
+//select a shows
+document.querySelector(`#showList`).addEventListener("input", function (e) {
+  //clear the current card episodes on the page ------------
+  document.querySelectorAll(`.card`).forEach((temp) => {
+    //if this element does not include the template class remove it
+    if (!temp.classList.contains(`template`)) temp.remove();
+  });
+
+  // console.log(this.value);
+  // add the new episodes to the page
+  getData(this.value);
+});
 
 //search support (start-of-word) using includes - search
-document.querySelector(`.s-name`).addEventListener("input", function (e) {
+document.querySelector(`.s-episodes`).addEventListener("input", function (e) {
   for (let episode of episodes) {
-    //if episode name - includes - this value
+    //if episode name - includes - this.value = the input
     if (episode.name.toLowerCase().includes(this.value.toLowerCase())) {
       //remove the - hide class - this knows it is a class so you do not need the `.`
       //Using querySelector with IDs that are numbers
@@ -79,34 +159,59 @@ document.querySelector(`.s-name`).addEventListener("input", function (e) {
       document.querySelector(`#episode` + episode.id).classList.add(`hide`);
     }
   }
+  updateDisplay();
 });
+// reset button
+function resetBtn() {
+  //remove the hide class
+  document.querySelectorAll(`.hide`).forEach((temp) => {
+    temp.classList.remove(`hide`);
+  });
+  updateDisplay();
+}
 
-//season selector
-document.querySelector(`.s-seasons`).addEventListener("input", function (e) {
-  for (let episode of episodes) {
-    //if seaseon has this value - using parseInt() function parses a string argument and returns an integer
-    if (episode.season === parseInt(this.value)) {
-      //remove the - hide class
-      document.querySelector(`#episode` + episode.id).classList.remove(`hide`);
-    } else {
-      //adds the - hide class
-      document.querySelector(`#episode` + episode.id).classList.add(`hide`);
-    }
-  }
-});
+//make a function to update the episodes displayed
+function updateDisplay() {
+  //subtract the number of episodes visible for the number of episodes which are invisible
+  total = document.querySelectorAll(`.card:not(.template)`).length;
+  hidden = document.querySelectorAll(`.hide`).length;
 
-//episode selector
-document.querySelector(`.s-episodes`).addEventListener("input", function (e) {
-  for (let episode of episodes) {
-    //if episode seaseon - includes - this value
-    if (episode.season === parseInt(this.value)) {
-      //remove the - hide class
-      document.querySelector(`#episode` + episode.id).classList.remove(`hide`);
-    } else {
-      //adds the - hide class
-      document.querySelector(`#episode` + episode.id).classList.add(`hide`);
-    }
-  }
-});
+  shown = total - hidden;
+  //look for visible episodes & total episodes (console)
+  document.querySelector(`.display`).value =
+    `Displaying ` + shown + `/ ` + total + ` episodes`;
+}
+
+// //season selector
+// document.querySelector(`.s-seasons`).addEventListener("input", function (e) {
+//   for (let episode of episodes) {
+//     //if seaseon has this value - using parseInt() function parses a string argument and returns an integer
+//     if (episode.season === parseInt(this.value)) {
+//       //remove the - hide class
+//       document.querySelector(`#episode` + episode.id).classList.remove(`hide`);
+//     } else {
+//       //adds the - hide class
+//       document.querySelector(`#episode` + episode.id).classList.add(`hide`);
+//     }
+//   }
+// });
+
+// //episode selector
+// document.querySelector(`.s-episodes`).addEventListener("input", function (e) {
+//   for (let episode of episodes) {
+//     //if episode season - includes - this value
+//     if (episode.season === parseInt(this.value)) {
+//       //remove the - hide class
+//       document.querySelector(`#episode` + episode.id).classList.remove(`hide`);
+//     } else {
+//       //adds the - hide class
+//       document.querySelector(`#episode` + episode.id).classList.add(`hide`);
+//     }
+//   }
+// });
+
+//400 show selector
+
+//500 Shows Listing selector
 
 window.onload = setup;
